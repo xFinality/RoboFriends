32daeaece26d5b21df8c8838ebe63d7dfc17ce14
Thomas Merat <phenix25@hotmail.fr>
2019-02-17 15:38:46 +0100

    commit robofriend

---
+++ build/precache-manifest.b536e9fbdddbdcdb065232382933e92f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2e6039d00f61dd969b2",
+    "url": "/static/js/main.b2e6039d.chunk.js"
+  },
+  {
+    "revision": "d1a417c44d4b1d0dc804",
+    "url": "/static/js/1.d1a417c4.chunk.js"
+  },
+  {
+    "revision": "b2e6039d00f61dd969b2",
+    "url": "/static/css/main.df3ed18e.chunk.css"
+  },
+  {
+    "revision": "d1a417c44d4b1d0dc804",
+    "url": "/static/css/1.858dc213.chunk.css"
+  },
+  {
+    "revision": "2635de95e569d2b7135c749197b5b68f",
+    "url": "/index.html"
+  }
+];
